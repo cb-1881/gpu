37b7b5d14f4ca9c7f4b860c6cec95a342d1c580a
cole <36876441+cb-1881@users.noreply.github.com>
2024-12-06 18:02:25 -0800

    Add files via upload

---
+++ gpu/vecadd_cpu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <math.h>
+#include <chrono> 
+// function to add the elements of two arrays
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+      y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1<<29; //512M
+
+  float *x = new float[N];
+  float *y = new float[N];
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+
+
+//start timer
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+
+  // Run kernel on 512M elements on the CPU
+  add(N, x, y);
+  //end timeer
+  std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+  //elapsed time calc
+  std::chrono::duration<double> elapsed = end_time - start_time;
+
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  delete [] x;
+  delete [] y;
+
+  return 0;
+}
